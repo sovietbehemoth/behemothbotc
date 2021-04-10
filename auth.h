91d7735a07c35af4b1b4b0bf52b557865fdb02e3
sovietbehemoth <73245562+sovietbehemoth@users.noreply.github.com>
2021-04-09 20:33:56 -0400

    Update auth.h

--- auth.h
+++ auth.h
@@ -2,7 +2,7 @@
 #define false 1
 #define true 0
 
-#define BOT_TOKEN "Mzc3NjEyMzAyNTIzODI2MTc3.YHDcbA.3qnUrD2FWTuHAsBlC0lLO828iHo"
+#define BOT_TOKEN "bhbumk"
 #define IS_BOT false
 
 #ifdef BOT_TOKEN
@@ -11,7 +11,7 @@
 #endif
 
 #if (IS_BOT == true)
-#define BOT_HEADERS "{ \"Content-Type\": \"application/json\", \"Authorization\": \"Bot Mzc3NjEyMzAyNTIzODI2MTc3.YHDcbA.3qnUrD2FWTuHAsBlC0lLO828iHo\" }"
+#define BOT_HEADERS "{ \"Content-Type\": \"application/json\", \"Authorization\": \"Bot ffwf\" }"
 #endif
 
 #endif
